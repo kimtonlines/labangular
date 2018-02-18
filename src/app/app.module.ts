d9536fad281fa0947fa5a149746d9cdcefc85ed0
Kim-Taing SIMIN <kimtonlines@gmail.com>
2018-02-18 19:03:00 +0000

    Création de Component liste-articles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { ListeArticlesComponent } from './liste-articles/liste-articles.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListeArticlesComponent
   ],
   imports: [
     BrowserModule
